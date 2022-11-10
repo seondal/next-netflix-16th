86f634d0cacd8a5ae15d4bfce07dfc95085e67f4
heesoo <11junghs@gmail.com>
2022-11-11 01:21:11 +0900

    test : home 페이지에 movie api 적용해보기

--- src/interfaces/interface.tsx
+++ src/interfaces/interface.tsx
@@ -3,3 +3,27 @@ export interface IFooter{
     name: string;
     selected: boolean;
 }
+
+export interface IMovieInfo{
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: number[];
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+    popularity: number;
+}
+
+export interface IData{
+    page: number;
+    results: IMovieInfo[];
+    total_pages: number;
+    total_results: number;
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,10 @@
 import Footer from "../../components/common/Footer"
 import styled from 'styled-components'
+import axios from 'axios';
+import { IData } from "../../interfaces/interface";
 
-export default function Home(){
+export default function Home(data:IData){
+    console.log(data);
     return (
         <Container>
             home
@@ -9,6 +12,17 @@ export default function Home(){
         </Container>
     )
 };
+
+export async function getServerSideProps(){
+    // Fetch data from external API
+    const res = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=0c75b9faeb39691a7d8e7af98ee5f303`)
+    const data = res.data;
+
+    console.log(data);
+
+    // Pass data to the page via props
+    return { props: { data } }
+}
 
 const Container = styled.div`
     background: blue;
