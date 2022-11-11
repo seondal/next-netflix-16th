52bf31738a7bbb294735443077bf236f876af2f9
seondal <sseondal@gmail.com>
2022-11-11 20:02:59 +0900

    feat : get top rated movies

--- src/api/Movies.tsx
+++ src/api/Movies.tsx
@@ -1,22 +1,32 @@
-import axios from 'axios';
-import {API_KEY} from '../assets/config';
+import axios from "axios";
+import { API_KEY } from "../assets/config";
 
 export const getNowPlaying = async () => {
-    const { data } = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`);
-    return data;
-}
+  const { data } = await axios.get(
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  return data;
+};
 
 export const getTopRated = async () => {
-    const {data} = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`);
-    return data;
-}
+  const { data } = await axios.get(
+    `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  return data;
+};
 
 export const getPopular = async () => {
-    const {data} = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
-    return data;
-}
+  const { data } = await axios.get(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  return data;
+};
 
 export const getUpComing = async () => {
-    const {data} = await axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`);
-    return data;
-}
+  const { data } = await axios.get(
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  return data;
+};
+
+export const getTopSearches = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`;
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,9 +1,32 @@
-import Footer from "../../components/common/Footer"
-export default function Search(){
-    return(
-        <>
-            Search
-            <Footer/>
-        </>
-    )
+import Footer from "../../components/common/Footer";
+import { useState, useEffect } from "react";
+import { getTopSearches } from "../../api/Movies";
+import { IMovieInfo } from "../../interfaces/interface";
+import Link from "next/link";
+
+export default function Search() {
+  const [movies, setMovies] = useState<IMovieInfo[]>([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (await fetch(getTopSearches)).json();
+      setMovies(results);
+    })();
+  }, []);
+  return (
+    <>
+      <input placeholder="Search for a show, movie, genre, e.t.c." />
+      <h2>Top Searches</h2>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <img
+            style={{ width: "10px" }}
+            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+          />
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
+      <Footer />
+    </>
+  );
 }
