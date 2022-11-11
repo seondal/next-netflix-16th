ca9643621e6bd4bf4670208a67869eff8fe61332
seondal <sseondal@gmail.com>
2022-11-12 02:37:02 +0900

    edit : footer styled-components 삭제

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,56 +1,46 @@
-import styled from "styled-components";
 import FooterItem from "./FooterItem";
 import FooterInfo from "../../assets/FooterInfo.json";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function Footer() {
   const router = useRouter();
   const footerInfo = FooterInfo;
 
-  const handleClick = (name: string) => {
-    if (name === "Home") {
-      router.push("/home");
-    } else if (name === "Search") {
-      router.push("/search");
-    }
-  };
-
   return (
-    <div className="container">
-      {footerInfo.map((f) => {
-        const select = router.pathname === "/" + f.alt;
-        return (
-          <ItemButton key={f.id} onClick={() => handleClick(f.name)}>
-            <FooterItem
-              key={f.id}
-              alt={f.alt}
-              name={f.name}
-              selected={select}
-            />
-          </ItemButton>
-        );
-      })}
-      <style jsx>
-        {`
-          .container {
-            position: fixed;
-            background: black;
+    <>
+      <div className="container">
+        {footerInfo.map((f) => {
+          return (
+            <Link href={`/${f.alt}`} key={f.id}>
+              <FooterItem
+                key={f.id}
+                alt={f.alt}
+                name={f.name}
+                selected={router.pathname === "/" + f.alt}
+              />
+            </Link>
+          );
+        })}
+      </div>
+      <style jsx>{`
+        .container {
+          position: fixed;
+          background: black;
 
-            // 중앙 정렬
-            left: 50%;
-            transform: translate(-50%, 0);
+          // 중앙 정렬
+          left: 50%;
+          transform: translate(-50%, 0);
 
-            bottom: 0;
+          bottom: 0;
 
-            width: 377px;
-            height: 48px;
+          width: 377px;
+          height: 48px;
 
-            display: flex;
-            justify-content: space-evenly;
-          }
-        `}
-      </style>
-    </div>
+          display: flex;
+          justify-content: space-evenly;
+        }
+      `}</style>
+    </>
   );
 }
-const ItemButton = styled.div``;
