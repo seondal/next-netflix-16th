b39d18b16eb5ccbf65c8ba6bf870e54787b84228
seondal <sseondal@gmail.com>
2022-11-12 00:19:57 +0900

    del : 불필요한 import 삭제

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,12 @@
 import "../../styles/globals.css";
 import type { AppProps } from "next/app";
-import {
-  Hydrate,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
 import React from "react";
 import Layout from "../components/common/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [queryClient] = React.useState(() => new QueryClient());
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </Hydrate>
-    </QueryClientProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
