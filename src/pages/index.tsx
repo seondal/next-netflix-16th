5c6ed75e554a4dcbe382d94b9ffa9ffa2912b2ee
heesoo <11junghs@gmail.com>
2022-11-13 13:23:51 +0900

    fix : logo 렌더링 lottie-web 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import { useRouter } from "next/router";
-import Image from "next/image";
-import Logo from "../../public/img/netflix_5.gif";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import lottie from "lottie-web";
 
 export default function Home() {
   const router = useRouter();
 
+  const netflixContainer = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
+
+    lottie.loadAnimation({
+      container: netflixContainer.current as HTMLDivElement,
+      renderer: 'svg',
+      loop: false,
+      autoplay:true,
+      animationData:require("../../public/img/netflix-logo.json")
+     
+    })
+
     const timer = setTimeout(() => {
       router.push("/home");
     }, 3100);
@@ -16,8 +27,22 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
-      <Image src={Logo} alt="Logo of Netflix" width={375} />
-    </div>
+    <>
+      <div className="container">
+        <div className="item" ref={netflixContainer}></div>
+      </div>
+      <style jsx>{`
+        .container {
+          justify-content: center;
+          align-items: center;
+        }
+        .item{
+          width: 375px;
+          height: 100%;
+          position: fixed;
+          z-index: 1000;
+        }
+      `}</style>
+    </>
   );
 }
