82cf435ed9fe6e0682687dcd43b0295c6bc82afd
heesoo <11junghs@gmail.com>
2022-11-16 16:06:04 +0900

    feat : 검색 기능 추가

---
+++ src/components/search/SearchList.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import { IMovieInfo } from "../../interface";
+
+interface SearchListProps {
+    movies: IMovieInfo[];
+}
+
+
+export default function SearchList(movies : SearchListProps){
+    return(
+        <>
+            {!movies && <h4>Loading...</h4>}
+            {/* {movies?.map((movie : any) => (
+                <Link
+                href={{
+                    pathname: `/movies/${movie.id}`,
+                    query: {
+                    title: movie.original_title,
+                    poster: movie.poster_path,
+                    overview: movie.overview,
+                    },
+                }}
+                as={`/movies/${movie.id}`}
+                key={movie.id}
+                >
+                <div key={movie.id}>
+                    <img
+                    style={{ width: "10px" }}
+                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                    />
+                    <h4>{movie.original_title}</h4>
+                </div>
+                </Link>
+            ))} */}
+          </>
+    )
+}
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,18 +1,36 @@
 import { getTopSearches } from "../api";
 import { IMovieInfo } from "../interface";
 import Link from "next/link";
+import SearchList from "../components/search/SearchList";
+import {useState, useEffect} from 'react';
 
 interface SearchProps {
   topSearchesMovies: IMovieInfo[];
 }
 
 export default function Search({ topSearchesMovies }: SearchProps) {
+
+  const [searchData, setSearchData] = useState(topSearchesMovies);
+  const [search, setSearch] = useState("");
+
+  const onChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  }
+
+  const filterData = searchData.filter((movie) => {
+    let result = movie.title.replace(" ","").toLocaleLowerCase().includes(search.replace(" ","").toLocaleLowerCase());
+    return result;
+  })
+
   return (
     <>
-      <input placeholder="Search for a show, movie, genre, e.t.c." />
+      <input 
+      type="text" value={search} onChange={onChange}
+      placeholder="Search for a show, movie, genre, e.t.c." />
       <h2>Top Searches</h2>
-      {!topSearchesMovies && <h4>Loading...</h4>}
-      {topSearchesMovies?.map((movie) => (
+      <SearchList movies={topSearchesMovies}/>
+      {!filterData && <h4>Loading...</h4>}
+      {filterData?.map((movie) => (
         <Link
           href={{
             pathname: `/movies/${movie.id}`,
