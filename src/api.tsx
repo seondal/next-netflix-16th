de6184855dca037e4c8c183159acac9ad8ae1cc4
seondal <sseondal@gmail.com>
2022-11-17 01:27:54 +0900

    [Docs] api.tsx 주석 추가

--- src/api.tsx
+++ src/api.tsx
@@ -1,14 +1,15 @@
 import { API_KEY } from "./assets/config";
+
 const BASE_URL = `https://api.themoviedb.org/3/`;
 
-
+// movie
 export const getNowPlaying = `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`;
 export const getTopRated = `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`;
 export const getPopular = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 export const getUpComing = `${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`;
 export const getTopSearches = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 
-export function getSearchMovies(search : string){
-    const result = `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${search}`;
-    return result;
+// search
+export function getSearchMovies(search: string) {
+  return `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${search}`;
 }
