e285cb5b2048dacabb0b7aaa2b38e60e88927679
heesoo <11junghs@gmail.com>
2022-11-16 20:24:20 +0900

    fix : 실시간 검색 수정

--- src/api.tsx
+++ src/api.tsx
@@ -6,7 +6,7 @@ export const getNowPlaying = `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&l
 export const getTopRated = `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`;
 export const getPopular = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 export const getUpComing = `${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`;
-export const getTopSearches = `${BASE_URL}/movie/top_rated?api_key=${API_KEY}`;
+export const getTopSearches = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 
 export function getSearchMovies(search : string){
     const result = `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${search}`;
--- src/components/search/SearchList.tsx
+++ src/components/search/SearchList.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { IMovieInfo } from "../../interface";
+import { GrCirclePlay } from "react-icons/gr";
 
 interface SearchListProps {
     movies: IMovieInfo[];
@@ -7,11 +8,14 @@ interface SearchListProps {
 
 
 export default function SearchList({movies} : SearchListProps){
-
     return(
         <>
             {!movies && <h4>Loading...</h4>}
-            {movies.map((movie : IMovieInfo) => (
+            {movies?.map((movie : IMovieInfo) => (
+                movie.backdrop_path == null
+                ?
+                <div key={movie.id}></div>
+                :
                 <Link
                 href={{
                     pathname: `/movies/${movie.id}`,
@@ -25,14 +29,46 @@ export default function SearchList({movies} : SearchListProps){
                 key={movie.id}
                 >
                 <div key={movie.id}>
-                    <img
-                    style={{ width: "10px" }}
-                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                    />
-                    <h4>{movie.original_title}</h4>
+                    <div className="container">
+                        <img className="poster"
+                            src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
+                        />
+                        <div className="title-box">
+                            <div className="title">
+                                {movie.original_title}
+                            </div>
+                            <div className="play">
+                                <GrCirclePlay/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 </Link>
             ))}
+            <style jsx>{`
+                .container {
+                    display: flex;
+                    width: 375px;
+                    margin-bottom: 3px;
+                }
+                .poster {
+                    object-fit: cover;
+                    width: 146px;
+                    height: 76px;
+                }
+                .title-box {
+                    background: #424242;
+                    width: 229px;
+                    height: 76px;
+                }
+                .title {
+                    font-weight: 400;
+                    font-size: 14.7222px;
+                }
+                .play {
+                    size: "25";
+                }
+            `}</style>
           </>
     )
 }
